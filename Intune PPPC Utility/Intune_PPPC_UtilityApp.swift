655886285242e684ef225eb76515d2f426fa6898
Gil Burns <gil@gilburns.com>
2026-02-22 21:37:55 -0600

    Initial Commit

---
+++ Intune PPPC Utility/Intune_PPPC_UtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intune_PPPC_UtilityApp.swift
+//  Intune PPPC Utility
+//
+//  Created by Gil Burns on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Intune_PPPC_UtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
