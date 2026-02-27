e3a8708507464fa8568c02cde7c40899307471df
Gil Burns <gil@gilburns.com>
2026-02-27 17:54:15 -0600

    Update Intune_PPPC_UtilityApp.swift

--- Intune PPPC Utility/Intune_PPPC_UtilityApp.swift
+++ Intune PPPC Utility/Intune_PPPC_UtilityApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct Intune_PPPC_UtilityApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        DocumentGroup(newDocument: PPPCDocument()) { file in
+            ContentView(document: file.$document)
+        }
+        .defaultSize(width: 980, height: 620)
+        .commands {
+            CommandGroup(replacing: .help) {
+                Divider()
+                Button("Intune PPPC Utility Wiki") {
+                    NSWorkspace.shared.open(
+                        URL(string: "https://github.com/gilburns/IntunePPPCUtility/wiki")!
+                    )
+                }
+                Button("Apple Privacy Settings Documentation") {
+                    NSWorkspace.shared.open(
+                        URL(string: "https://developer.apple.com/documentation/devicemanagement/privacypreferencespolicycontrol")!
+                    )
+                }
+                Button("Apple Platform Deployment - Privacy Preferences Policy Control") {
+                    NSWorkspace.shared.open(
+                        URL(string: "https://support.apple.com/en-us/guide/deployment/dep38df53c2a/web")!
+                    )
+                }
+            }
         }
     }
 }
